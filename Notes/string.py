1dd2ac685e2f4637f1891d6341e7e560448c46c9
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-08 14:42:40 -0600

    did some string notes

--- Notes/string.py
+++ Notes/string.py
@@ -1,2 +1,36 @@
 #AK 7th string notes
 
+#What makes something a string?
+#a collection of letters, numners, and symbols that are surrounded by quotation marks
+#Why do we have strings?
+
+#How do stupid proofing and sanitization relate to each other?
+
+#What is debugging?
+
+#How do you debug the different types of errors?
+
+#Describe what each of the methods below does:
+#find()
+#concatenate (add one string to another)
+#upper(to mke all letters uppercase)
+#lower(to make all letters lowercase)
+#strip(to remove the spaces at the beginning and end of a string)
+
+#" content "
+#'content'
+#they are the same
+
+#examples
+first_name = input("what is your first name?:\n").strip().title()
+last_name = input("what is your last name?:\n").strip().title()
+full_name = first_name + " " + last_name
+
+sentence = 'The quick brown fox jumps over the lazy dog.'
+print(sentence)
+
+
+print("welcome to my program", full_name + "!")
+
+
+
