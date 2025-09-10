ddfa5cb232e8ca54e9ae6cc1c33aad826b869d26
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-10 14:25:46 -0600

    finished silly sentences

---
+++ Pratices/silly_sentences.py
@@ -0,0 +1,20 @@
+# AK 7th silly sentences
+
+adjective1 = input("pick an adjective:\n")
+adjective2 = input("pick another adjective:\n")
+type_of_bird = input("pick a type of bird:\n")
+room_in_a_house = input("pick a room in a house:\n")
+verb_past_tense = input("pick a verb (past tense):\n")
+verb = input("pick a verb:\n")
+relative_name = input("pick a relative's name:\n")
+noun1 = input("pick a noun:\n")
+liquid = input("pick a type of liquid:\n")
+verb_ing = input("pick a verb ending in -ing:\n")
+body_part_plural = input("pick a plural body part:\n")
+noun2 = input("pick another noun:\n")
+
+print("It was a " + adjective1 + ", cold November day. I woke up to the " + adjective2 + " "
+"smell of " + type_of_bird + " roasting in the " + room_in_a_house + " downstairs. I " + verb_past_tense + 
+" down the stairs to see if I could help " + verb + " the dinner. My mom said, 'See if " + relative_name + " needs a fresh " + 
+noun1 + ".' So I carried a tray of glasses full of " + liquid + " into the " + verb_ing + " room. When I got there, I couldn't believe my " + 
+body_part_plural + "! There were " + noun2 + " on the floor!")    
