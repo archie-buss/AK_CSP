79e0cfd34f4a0e8f24bf422d307f2892a9b680c1
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-10 14:31:20 -0600

    finished silly name

---
+++ Pratices/name_decorator.py
@@ -0,0 +1,7 @@
+# AK 7th name decorator
+
+name= input("what is your name?\n")
+right_decor = input("Enter a decoration going right:\n")
+left_decor = input("Enter another decoration goiing left:\n")
+
+print(left_decor + name + right_decor)
