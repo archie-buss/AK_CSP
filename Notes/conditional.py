537e2650444c40de618957bac03ef9285e4f3db3
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-17 14:25:24 -0600

    did some notes on conditionals

--- Notes/conditional.py
+++ Notes/conditional.py
@@ -2,16 +2,31 @@
 
 
 #What do if statements do?
+# telling the computer "if something happens, do this"
 #What are boolean statements? 
+# always a true or false
 #What do else statements do?
+
 #What kind of statement do you use if you have more than 2 needed outcomes?
+
 #What do each of the different symbols mean in conditionals?
+
 #<
 #>
 #<=
 #>=
-#==
+#== : is a question
 #!
 #What are the 3 logical operators?
+
 #What are logical operators for?
+
 #What does a nested conditional statement do?
+
+
+homework = "yes"
+if homework == "no":
+    print("Then go do your homework!")
+
+ else:
+    print("Yes you can go!")cccc
