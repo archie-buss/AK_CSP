b91da1d70d428f2be9826b22560b5cf4792f8e70
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-18 14:36:35 -0600

    finished old enough assignment

---
+++ Pratices/old_enough.py
@@ -0,0 +1,14 @@
+#AK 7th old enough
+
+
+
+age = int(input("Hi, what is your age?\n"))
+
+if age >= 18:
+    print(f"you are {age}, you can vote!")
+elif age >= 15:
+    print(f"you are {age}, you can get your learns permit!")
+elif age <= 15:
+    print(f"you are {age}, you can go to school!")
+else:
+    print(f"you are {age}, your not old enough, go home!")
