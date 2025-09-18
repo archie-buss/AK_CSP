1d1d98d77248a3e3f4cf16ba2ef7e2c394b00fcc
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-18 14:40:36 -0600

    did some work on old enough

--- Pratices/old_enough.py
+++ Pratices/old_enough.py
@@ -6,6 +6,8 @@ age = int(input("Hi, what is your age?\n"))
 
 if age >= 18:
     print(f"you are {age}, you can vote!")
+elif age >= 16:
+    print(f"you are {age}, you can drive!")
 elif age >= 15:
     print(f"you are {age}, you can get your learns permit!")
 elif age <= 15:
