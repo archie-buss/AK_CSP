76a181d181d2724ac6e4367c644f3657aa6acbbf
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-19 14:34:04 -0600

    finished time practice

---
+++ Notes/lists.py
@@ -0,0 +1,6 @@
+#AK 7th lists notes
+
+
+#complex data types contain more data types inside
+
+siblings = ["Sariah", "Hilary", "Isabella","Mysaih","Liberty",[ "Zenos", "Sara", "Deoseph", "Nicron", ""]]
---
+++ Pratices/Getting_time_practice.py
@@ -0,0 +1,11 @@
+#AK 7th getting time practice
+
+
+time = int(input("what time is it? Make sure its in military time?\n"))
+
+if time <= 12:
+    print("Good morning!")
+elif time <= 17:
+    print("Good afternoon!")
+else:
+    print("Good evening")
