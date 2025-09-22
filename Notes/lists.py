262d235a3fd36bd5b554ddece41a0bab5fe43a64
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-22 14:42:27 -0600

    Finished loops notes

--- Notes/lists.py
+++ Notes/lists.py
@@ -3,4 +3,11 @@
 
 #complex data types contain more data types inside
 
-siblings = ["Sariah", "Hilary", "Isabella","Mysaih","Liberty",[ "Zenos", "Sara", "Deoseph", "Nicron", ""]]
+siblings = ["Sariah", "Hilary", "Isabella","Mysaih","Liberty", "Archie"[ "Zenos", "Sara", "Deoseph", "Nicron", "emrys", "tio"]]
+
+siblings[0] = "Sariah"
+siblings[4] = "Mysiah"
+siblings[2] = "Isabella"
+siblings[1] = "Hilary"
+siblings.remove("Archie")
+print(siblings)
--- Notes/loops_notes.py
+++ Notes/loops_notes.py
@@ -2,15 +2,73 @@
 
 
 #What is a loop? 
-
+    #Repeats the code inside for a specified numbers of times
 #What are the two types of loops?
-
+    # for loops
+    # while loop#
 #What is iteration?
-
+    # how many times the loop has run?
 #What are lists? 
-
+ # A list of things for the code
 #How do you make lists in python? 
-
+ # by making a list and making it run
 #How do you make for loops in python? 
-
+ 
 #How do you make while loops in python?
+# by putting while in font of your code
+
+
+# for loop
+#foods = ["Chocolate", "Pasta", "Brownies", "Cookies", "Garlic", "Bread", "Fajitas"]
+#for food in foods:
+    #print(f"{food} is my favorite food!")
+
+#for x in range(1,20):
+    #print(x)
+
+# while loop
+
+#i = 0
+#while True:
+        #print(i)
+        #i+= 1#
+#Infinite loop !!!BAD!!!(missed one of the 3 steps)
+
+
+#correct while loop
+#i = 1
+
+
+#while i < 20:
+    #print(i)
+    #i+=1
+
+#x = 1
+
+#hile x < 21:
+    #if x % 2 == 0:
+        #print(f"{x} is an even number")
+
+    #else:
+        #print(f"{x} is an odd number")
+    #x += 1
+
+#import random
+
+#d = 1
+
+#end = random.randint(1,20)
+
+#while d != end:#
+    #print("Duck")
+    #d += 1
+
+#print("Goose")
+
+#while True:
+    #if d == end:
+        #print("Goose")
+        #break
+   #else:
+        #print("duck")
+        #d += 1#
