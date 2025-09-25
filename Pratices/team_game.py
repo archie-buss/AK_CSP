c08cc021050c2109363e26eecea4c0b2edbffc9a
archie-buss <archeantus.kamore@ucas-edu.net>
2025-09-25 14:42:05 -0600

    yes

---
+++ Pratices/team_game.py
@@ -0,0 +1,27 @@
+#AK 7th team game
+
+print("The crazy adventures of Ms. LaRosa")
+name = input("What is your name\n")
+print(f"Welcome {name} to The crazy adventures of Ms. LaRose!")
+print("you come to a crossroad and you can go Forward, Left, or Right\n")
+direction_1 = input("Forward, Left, or Right").capitalizelize().strip()
+
+
+
+
+
+
+
+# archie
+print("you went right!")
+print(f"{name}, you fell into a portal and teleport to the midieval times!")
+print("you see Mr. LaRosa milking a cow, what will you do?")
+
+print("left = you help her")
+print("right = you push the cow over")
+print("middle = you leave her alone")
+
+x = 1
+
+if x == 1:
+    print(1)
