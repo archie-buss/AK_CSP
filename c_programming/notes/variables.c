81f5f399b12fd373ec669df1e3d2bfa6eea1f2fc
archie-buss <Archeantus.kamore@ucas-edu.net>
2025-10-03 14:42:28 -0600

    finished varibales notes

--- c_programming/notes/variables.c
+++ c_programming/notes/variables.c
@@ -2,13 +2,23 @@
 #include <stdio.h>
 
 int main(void){
-    int grade = 95; // 4 bytes
+    int grade; // 4 bytes
     float pi = 3.14; //4 bytes
     double long_pi = 3.141592653; //8 bytes
-    char letter_grade = 'A'; //1 byte
-    char name[] = 'archie'; 
-    printf("%s did it!", name);
-    printf("You hvae a %d, in the class. That is an %c", grade, letter_grade);
+    char letter_grade; //1 byte
+    char name[] = "archie"; 
+
+     //User input
+    printf("What is your grade percentage as a whole number?:\n");
+    scanf("%d", &grade); 
+
+
+    printf("What is letter grade?:\n");
+    scanf(" %c", &letter_grade);
+
+
+    printf("%s did it!\n", name);
+    printf("You have a %d, in the class. That is an %c\n", grade, letter_grade);
 
 
     return 0;
@@ -21,25 +31,20 @@ int main(void){
 //List three common data types used in C and their corresponding format specifiers for printf().
 // : strings %s, integers %d, characters %c
 //How do you declare and initialize an integer variable named "age" with the value 25 in C?
-// : 
+// :  int age = 25;
 //What is the difference between printf() and scanf() functions in C?
-// :
-//Write a C program that asks the user for their name and age, then prints a greeting message with their information.
-// :
+// : printf is for output, scanf is for input 
 //How do you add comments in C? What are the two types of comments?
-// :
+// : // single line // multi line /* */
 //What is the purpose of the #include <stdio.h> line at the beginning of a C program?
-// :
+// : standard library, input output. To be able to use.
 //In C, what is the significance of the main() function?
-// :
-//Write a C program that calculates the area of a rectangle. Ask the user to input the length and width, then display the result.
-// :
+// : main function is where the program starts
 //What is the difference between %d and %f format specifiers in printf()?
-// :
+// : %d is for integers, %f is for floating point numbers
 //How do you print a newline character in C?
-// :
-//Write a C program that converts temperature from Celsius to Fahrenheit. Ask the user for input in Celsius and display the result in Fahrenheit.
-// :
+// : \n
 //What is the purpose of the & symbol when using scanf() to get user input?
-// :
+// : It is used to get the address of the variable
 //How do you declare a constant in C? Provide an example.
+// :const. sets the variable to be unchangeable.
