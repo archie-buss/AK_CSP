04d0c3ec340b78d089748931faaf9d7e595b8b76
archie-buss <Archeantus.kamore@ucas-edu.net>
2025-10-10 14:42:08 -0600

    finished a lot

---
+++ c_programming/practices/Old_enough.c
@@ -0,0 +1,20 @@
+//AK 7th old enough practice 
+
+#include <stdio.h>
+
+int main(void) {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if (age >= 18) {
+        printf("You are old enough to vote.\n");
+        printf("You are old enough to drive.\n");
+    } else if (age >= 15) {;
+        printf("You are old enough to get a learners permit.\n");
+    } else if (age >= 5) {
+        printf("You are old enough to go to school.\n");
+    } else {
+        printf("you aren't old enought to do anything do back to bed you baby.\n");
+    }
+    return 0;
+}
