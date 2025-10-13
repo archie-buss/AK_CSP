d75ca941d62f26abc8921390da1051e6c14ab844
archie-buss <Archeantus.kamore@ucas-edu.net>
2025-10-13 14:04:40 -0600

    did alot

---
+++ c_programming/notes/Loops.c
@@ -0,0 +1,33 @@
+//AK 7th  loops notes
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void) {
+    int nums[] = {1,2,3,4,5,6,7};
+
+    for(int x = 0; x < 7; x++) {
+        printf("%d\n", nums[x]);
+    }
+
+
+    return 0;
+}
+
+
+//What is a loop? 
+//; 
+//What are the two types of loops?
+
+//What is iteration
+
+//What are arrays? 
+
+// loops but a different name
+//How do you make lists in C?
+//  
+//How do you make for loops in C? 
+//
+//How do you make while loops in C?
+//
