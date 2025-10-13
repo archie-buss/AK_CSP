be19b8469c8d31f9ea5ffd661bf7a7b1a1f997d6
archie-buss <Archeantus.kamore@ucas-edu.net>
2025-10-13 14:09:57 -0600

    yes

--- c_programming/notes/Loops.c
+++ c_programming/notes/Loops.c
@@ -6,12 +6,15 @@
 
 int main(void) {
     int nums[] = {1,2,3,4,5,6,7};
+    char chandies[5][20] = {"KitKat", "skittles", "snickers", "reeses", "twix"};
 
     for(int x = 0; x < 7; x++) {
         printf("%d\n", nums[x]);
     }
 
-
+    for (int i = 0; i < 5; i++) {
+        printf("%s\n", chandies[i]);
+    }
     return 0;
 }
 
