7a50d3475b2b3f29b1143dabfd1933116d5c0f65
archie-buss <Archeantus.kamore@ucas-edu.net>
2025-10-13 14:18:08 -0600

    finished loops notes

--- c_programming/notes/Loops.c
+++ c_programming/notes/Loops.c
@@ -6,31 +6,39 @@
 
 int main(void) {
     int nums[] = {1,2,3,4,5,6,7};
-    char chandies[5][20] = {"KitKat", "skittles", "snickers", "reeses", "twix"};
+    char candies[5][20] = {"KitKat", "skittles", "snickers", "reeses", "twix"};
 
     for(int x = 0; x < 7; x++) {
         printf("%d\n", nums[x]);
     }
 
     for (int i = 0; i < 5; i++) {
-        printf("%s\n", chandies[i]);
+        printf("%s\n", candies[i]);
+    }
+    for (int num = 1; num < 11; num++){
+        printf("%d\n", num);
+    }
+
+    int n = 0;
+    while(n < 11){
+        printf("%d\n", n);
+        n++;
     }
     return 0;
 }
 
 
 //What is a loop? 
-//; 
+// A way to repeat code multiple times
 //What are the two types of loops?
-
+// for loops and while loops
 //What is iteration
-
+// repeating code multiple times
 //What are arrays? 
-
-// loops but a different name
+// lists of data
 //How do you make lists in C?
-//  
+// using arrays
 //How do you make for loops in C? 
-//
+// using the for keyword
 //How do you make while loops in C?
-//
+// using the while loop
